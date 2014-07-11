6b4347203c30865084275c5c198b766117ce92fd
XavierF <xavierf424@gmail.com>
2014-07-11 15:22:10 -0700

    css and template updates

--- home-page-template.php
+++ home-page-template.php
@@ -9,15 +9,66 @@
 <?php get_header(); ?>
 
 <section id="hero" role="hero-unit">
-
-
-	 
+	<div class="container-fluid">
+		<h1 class="tag"><?php $site_description = get_bloginfo( 'description', 'display' ); echo "$site_description";?></h1>
+	</div>
 </section> <!-- end hero-unit -->
 
+
 	<div class="container">
+
+
+<div id="content" class="wrap clearfix">
+
+						<div id="main" class="clearfix" role="main">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+									
+
+
+								</header> <!-- end article header -->
+
+								<section class="entry-content clearfix" itemprop="articleBody">
+									<?php the_content(); ?>
+							</section> <!-- end article section -->
+
+
+							
+
+							</article> <!-- end article -->
+
+							<?php endwhile; else : ?>
+
+									<article id="post-not-found" class="hentry clearfix">
+										<header class="article-header">
+											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+										<section class="entry-content">
+											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+						</div> <!-- end #main -->
+
+				</div> <!-- end #content -->
+
+
 		<div class="row">
 			<div class="col-sm-12">
-				<h1 class="tag"><?php $site_description = get_bloginfo( 'description', 'display' ); echo "$site_description";?></h1>
+				
 			</div>
 		</div>
 	</div>
@@ -27,8 +78,17 @@
 
 		<div class="row content">
 			
-			<div class="col-md-4">
-				
+			<div class="col-md-3">
+
+				<?php the_field('left'); ?>
+      </div><!-- end .col-md-4 -->
+      <div class="col-md-3">
+
+				<?php the_field('middle'); ?>
+      </div><!-- end .col-md-4 -->
+      <div class="col-md-3">
+
+				<?php the_field('right'); ?>
       </div><!-- end .col-md-4 -->
 
 		</div><!-- .row -->
