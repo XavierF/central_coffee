d68d95ab906406ac50630839b3f7178dd4a4fd95
XavierF <xavierf424@gmail.com>
2016-05-03 15:13:45 -0700

    home page main content update

--- full-width-home-slider.php
+++ full-width-home-slider.php
@@ -24,20 +24,20 @@
 	<section class="container clearfix" id="main-content">
 		<div class="row">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				
 				<article id="post-<?php the_ID(); ?>" <?php post_class( 'main-column col-md-12 clearfix' ); ?> role="main" itemscope itemtype="http://schema.org/BlogPosting">
-			<section>
-				<h1 class="site-tagline"><?php bloginfo('description'); ?></h1>
-				<hr class="dotted">
-				<?php the_content(); ?>
-	 		</section>
-	 		<footer class="article-footer">
-						<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ' ', '' ); ?>
-					</footer> <!-- end article footer -->
+					<section>
+						<h1 class="site-tagline"><?php bloginfo('description'); ?></h1>
+						<hr class="dotted">
+						<?php the_content(); ?>
+			 		</section>
+		 		<footer class="article-footer">
+							<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ' ', '' ); ?>
+				</footer> <!-- end article footer -->
 
 				</article> <!-- end article -->
 
 		<?php endwhile; else : ?>
-
 				<article id="post-not-found" class="hentry clearfix">
 					<header class="article-header">
 						<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
@@ -49,12 +49,10 @@
 							<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
 					</footer>
 				</article>
-
 			<?php endif; ?>
-
-
 		</div><!-- .row -->
-	</section>
+	</section><!-- #main-content -->
+
 	<section class="film-strip clearfix">
 		<?php if( have_rows('slides') ): ?>
 		<div class="slides">
